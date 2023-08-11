3e4d12fd038082d96d822f4039b53ad524301829
Dennis E. Wekesa <dennis.e.wekesa@gmail.com>
2023-08-12 01:53:28 +0300

    Update primary key generation type on Transaction Entity

--- src/main/java/co/spektra/api/transaction/Transaction.java
+++ src/main/java/co/spektra/api/transaction/Transaction.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @Setter
 public class Transaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "sender_name", nullable = false)
