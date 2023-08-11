737255204329a950d0ad0e8ea127eadfe074d8e9
Dennis E. Wekesa <dennis.e.wekesa@gmail.com>
2023-08-12 00:10:50 +0300

    Initial commit

---
+++ src/test/java/co/spektra/transactionapi/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.spektra.transactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
