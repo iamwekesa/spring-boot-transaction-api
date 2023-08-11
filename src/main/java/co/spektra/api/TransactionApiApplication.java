e4d0b38d75d33d69fafdc43ab29b8e611ba8add0
Dennis E. Wekesa <dennis.e.wekesa@gmail.com>
2023-08-12 01:19:15 +0300

    Task 1: Create Entity and JPA Repository, update package name

--- src/main/java/co/spektra/api/TransactionApiApplication.java
+++ src/main/java/co/spektra/api/TransactionApiApplication.java
@@ -1,4 +1,4 @@
-package co.spektra.transactionapi;
+package co.spektra.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/co/spektra/api/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package co.spektra.api.transaction;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transaction")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "sender_name", nullable = false)
+    private String senderName;
+
+    @Column(name = "receiver_name", nullable = false)
+    private String receiverName;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDate transactionDate;
+}
